ae773cd3a50e194f46ee36f78fb995e575adc2e3
葛贵阳 <945785319@qq.com>
2020-09-28 08:39:17 +0800

    9_25

---
+++ study9_25/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace study9_25
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("================哈希表==================");
+            Hashtable hash = new Hashtable();//穿件实例
+            //添加键-值对
+            hash.Add("key1", "value1");
+            hash.Add("key2", "value2");
+            //修改键-值对
+            hash["key1"] = "value01";
+            foreach (var item in hash.Keys)
+            {
+                Console.WriteLine(item + ":" + hash[item] + "\n");
+            }
+            Console.WriteLine("================哈希表==================");
+            //  DictionaryEntry公开枚举器
+            foreach (DictionaryEntry item in hash)
+            {
+                Console.WriteLine("key={0},value={1}", item.Key, item.Value);
+            }
+
+            Console.WriteLine("================哈希表练习==================");
+            Hashtable hashtext = new Hashtable();
+            hashtext.Add(".txt", "记事本.exe");
+            hashtext.Add(".bmp", "图片管理器.exe");
+            hashtext.Add(".doc", "officeWorld.exe");
+            hashtext.Add(".xls", "officeExcle.exe");
+            hashtext[".doc"] = "wpf";
+            foreach (var item in hashtext.Keys)
+            {
+                Console.WriteLine("key={0},value={1}", item, hashtext[item]);
+            }
+            Console.WriteLine("================字典==================");
+            Dictionary<string, string> dict = new Dictionary<string, string>();
+            dict.Add("key1", "value1");
+            dict.Add("key2", "value2");
+            dict.Add("key3", "value3");
+            dict.Add("key4", "value4");
+            dict.Add("key5", "value5");
+            dict["key3"] = "value03";
+            foreach (var item in dict.Keys)
+            {
+                Console.WriteLine(item + ":" + dict[item] + "\t");
+            }
+            foreach (KeyValuePair<string,string> item in dict)
+            {
+                Console.WriteLine("key={0},value={1}", item.Key, item.Value);
+            }
+
+
+
+
+
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ study9_25winfrom/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace study9_25winfrom
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("中午好");
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            this.button2.Click -= new System.EventHandler(this.button2_Click);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
